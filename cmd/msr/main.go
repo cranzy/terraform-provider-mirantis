807142f2bd07f499e9f5964dfb9dd5aab7ab110b
James Nesbitt <jnesbitt@mirantis.com>
2022-04-28 09:42:33 -0400

    PRODENG-1697 fmt changes to cmd/msr (#34)
    Signed-off-by: James Nesbitt <jnesbitt@mirantis.com>

--- cmd/msr/main.go
+++ cmd/msr/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-  "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-  "github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
-  "github.com/Mirantis/terraform-provider-mirantis/mirantis/msr"
+	"github.com/Mirantis/terraform-provider-mirantis/mirantis/msr"
 )
 
 func main() {
-  plugin.Serve(&plugin.ServeOpts{
-    ProviderFunc: func() *schema.Provider {
-      return msr.Provider()
-    },
-  })
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return msr.Provider()
+		},
+	})
 }
